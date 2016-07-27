8b2c5eda2845f0463b2e23fdc351b29559518f16
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-07-27 14:31:42 -0600

    Create sessions

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |new_user|
+      new_user.uid                = auth_info.uid
+      new_user.name               = auth_info.extra.raw_info.name
+      new_user.handle             = auth_info.extra.raw_info.screen_name
+      new_user.avatar             = auth_info.extra.raw_info.profile_image_url
+      new_user.bio                = auth_info.extra.raw_info.description
+      new_user.oauth_token        = auth_info.credentials.token
+      new_user.oauth_secret       = auth_info.credentials.secret
+      new_user.location           = auth_info.extra.raw_info.location
+    end
+  end
+end
