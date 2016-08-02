9974144037f17b84090762b34c6647655be8a6e7
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-08-02 16:36:06 -0600

    Create import.rake to import all shows from database, thereby making responses much faster Create methods website and domain to make show show page work correctly (as wasn't finding the correct emails for most podcasts due to http://www

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,26 +1,26 @@
 class Show < OpenStruct
-  attr_reader :show
+  attr_reader :id, :title, :description, :number_of_episodes, :website
   def initialize(show)
-    @show = show
+    @id = show["id"]
+    @title = show["title"]
+    @description = show["description"]
+    @number_of_episodes = show["number_of_episodes"]
   end
 
   def website
-    if show.web_profiles.present?
-      show.web_profiles.personal.url || show.web_profiles.other.url
+    if show["web_profiles"]["personal"].present?
+      show["web_profiles"]["personal"]["url"]
+    elsif show["web_profiles"]["other"].present?
+      show["web_profiles"]["other"]["url"]
     end
   end
 
-  def self.all_shows
-    response = AudiosearchServices.new.all_shows
-    response.map do |show|
-      Show.new(show)
-    end
+  def domain
+    website.gsub("http://www.","")
   end
-
-  #Try to pull in all the information
-  #don't know how to pull in all information
-  #task query - only save if not an error
   # rake run background task (seed_database) - rake task: heroku scheduler. Won't even have to create a service. Let a cron scheduler. https://github.com/popuparchive/audiosearch-cookbook/wiki/Searching-Shows
   #database_audiosearch_service, dynamic_audiosearch_service, both inheriting from base_audiosearch_service
   #Show.new(show) in the rake task
+  #squash assets to get it up to heroku
+  #
 end
