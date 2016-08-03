babef51d19db814fb984cd233aac1a1b1b468065
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-08-03 12:11:42 -0600

    Got the custom font to work without requiring the font to be downloaded locally.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,18 @@ $(document).ready(function() {
       interval: 2400
     })
   });
+
+  $(function () {
+  var w = $(window);
+  w.scroll(function () {
+    if (w.scrollTop() !== 0) {
+      $(".d").removeClass("visible");
+      return;
+    }
+
+    $(".d").addClass("visible");
+  });
+
+  $(".d").addClass("visible");
+
+});
