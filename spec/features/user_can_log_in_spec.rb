2bab73e896d23d6d8b916d4de8c5dee075d3ae81
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-08-04 10:09:33 -0600

    Fix things to deploy to heroku

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -1,1 +1,23 @@
 require "rails_helper"
+RSpec.feature "Twitter login is possible" do
+  context "user goes to the root path" do
+    scenario "User sees a page that allows for login and logo" do
+      visit root_path
+      expect(page).to have_content("Login")
+      expect(page).to have_content("Cast-Away")
+      expect(page).to have_button("Search")
+      FactoryGirl.create_list(:show, 20)
+      fill_in :query, with: "4"
+      click_on "Search"
+
+      expect(current_path).to eq(shows_search_path)
+      expect(page).to have_content('0 search results for "4"')
+    end
+  end
+
+  scenario "User can view what's trending from the homepage" do
+    visit root_path
+    expect(page).to have_button("What's Trending")
+  end
+
+end
