e0a13c063c30ec0fdf0302306b05d3e8b466b705
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-08-04 02:26:56 -0600

    Add ability to check out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
    if !current_user
      button_to("Log in to purchase advertisements", login_path(previous_page: "cart" ), method: :get, class: "btn btn-custom btn-person")
    else
-     button_to("Checkout", new_charge_path, method: :get, class: "btn btn-custom btn-person")
+     button_to("Checkout", new_charge_path, method: :get, class: "btn checkout-button")
    end
   end
 end
